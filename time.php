fa254a236461f02fc0d852579670996c1ae58a36
JEGEDE <david_jegede766@yahoo.com>
2018-04-11 19:05:06 +0100

    initial commit

---
+++ time.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+</head>
+
+<body>
+    
+    <style>background-color: #ff0000;</style>
+
+<p>Date/Time: MM/DD/YYYY hh:mm<br>
+<?php echo(strftime("%m/%d/%Y %H:%M")); ?></p>
+
+
+</body>
+
+</html>
